f97d7143ca74dfb5a072a1de8d998d010cc801ae
Karachik <c.kiselev2014@yandex.ru>
2023-09-16 16:59:36 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,134 @@
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main(){
+//    // Коментарий
+//    printf("Hello world");
+//
+//    return 0;
+//    /* Многострочный коментрий
+//     *
+//     */
+//
+//    int a;
+//    float b;
+//    float x = 4.56;
+//    char c, c2 = 'A', m;
+//    int p;
+//    p = 23;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int a,b,c; //объявление переменных
+//    printf("Введите два целых числа\n");
+//    scanf("%d%d", &a, &b);
+//    c = a + b;
+//    printf("Результат %d + %d = %d \n", a, b, c);
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int x, n;
+//    float d;
+//    x = 7;
+//    d = x / 4;
+//    printf("Резльтат первого выполнение: %f \n", d);
+//    d = x / 4.;
+//    printf("Резльтат второго выполнение: %f \n", d);
+//    d = (float) x/4;
+//    printf("Резльтат третьего выполнение: %f \n", d);
+//    n = 7. / 4.;
+//    printf("Резльтат четвертого выполнение: %d \n", n);
+//}
+//
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//    int x, n;
+//    float d;
+//    x = 7;
+//    d = x / 4;
+//    printf("Резльтат первого выполнение: %f \n", d);
+//    d = x / 4.;
+//    printf("Резльтат второго выполнение: %f \n", d);
+//    d = (float) x/4;
+//    printf("Резльтат третьего выполнение: %f \n", d);
+//    n = 7. / 4.;
+//    printf("Резльтат четвертого выполнение: %d \n", n);
+//
+//    int mod = 10%7;
+//    printf("mod = %d", mod);
+//
+//    float z;
+//    z = (d - 5 * x) - fabs(32 - 120) * sqrt(56);
+//
+//    int p = 0;
+//    p++;
+//    ++p;
+//    p--;
+//    --p;
+//
+//    z = 15 * p++;
+//
+//    z -= a;
+//}
+
+//#include <stdio.h>
+//
+//main() {
+//    //1
+//    float a, b;
+//    printf("Введите два числа: \n");
+//    scanf("%f%f", &a, &b);
+//    if (a>b){
+//        printf("a больше b");
+//    }else if (a==b){
+//        printf("a равно b");
+//    }else{
+//        printf("a меньше b");
+//    }
+//    //2
+//    int age;
+//    scanf("%d", &age);
+//    if (25 <= age && age <= 40){
+//        // Альтернатива &
+//
+//    }else{
+//
+//
+//    }
+//
+//    if (25 <= age || age <= 40){
+//        // Альтернатива |
+//    }else{
+//
+//    }
+//    //3
+//    char c;
+//
+//    scanf("%c", &c);
+//
+//    switch(c){
+//        case 'A':
+//        case 'a':
+//            printf("Антилопа\n");
+//            break;
+//        case 'b':
+//            printf("sddfsfg\n");
+//            break;
+//        default:
+//            printf("неизвестная буква");
+//    }
+//}
+
---
+++ sem1.h
@@ -0,0 +1,14 @@
+//
+// Created by Kirill Kiselev on 04.09.2023.
+//
+
+#ifndef UNTITLED_SEM1_H
+#define UNTITLED_SEM1_H
+
+
+class sem1 {
+
+};
+
+
+#endif //UNTITLED_SEM1_H
